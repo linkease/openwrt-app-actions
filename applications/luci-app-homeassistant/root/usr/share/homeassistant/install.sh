c39201799407acf0aee481616506441bcafd5d9d
YGAS <ycl310397039@hotmail.com>
2021-09-23 14:11:52 +0800

    update home assistant

--- applications/luci-app-homeassistant/root/usr/share/homeassistant/install.sh
+++ applications/luci-app-homeassistant/root/usr/share/homeassistant/install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-rm -rf /opt/docker/hassio
-
-docker run -d --name hassio_supervisor --privileged \
--v /var/run/docker.sock:/var/run/docker.sock \
--v /var/run/dbus:/var/run/dbus \
--v /opt/docker/hassio:/data \
--e SUPERVISOR_SHARE="/opt/docker/hassio" \
--e SUPERVISOR_NAME=hassio_supervisor \
--e HOMEASSISTANT_REPOSITORY="homeassistant/qemuarm-64-homeassistant" \
---restart unless-stopped homeassistant/aarch64-hassio-supervisor
+docker run -d \
+>   --name homeassistant \
+>   --privileged \
+>   --restart=unless-stopped \
+>   -e TZ="Asia/Shanghai" \
+>   -v /root/homeassistant/config:/config \
+>   --network=host \
+>   homeassistant/home-assistant:latest
