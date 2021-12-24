8441e48eeae4527b6ffc2ccf53db6f1253ffd91b
YGAS <ycl310397039@hotmail.com>
2021-12-24 13:10:32 +0800

    add luci-app-ubuntu

---
+++ applications/luci-app-ubuntu/luasrc/model/cbi/ubuntu/status.lua
@@ -0,0 +1,26 @@
+local m, s
+local uci = luci.model.uci.cursor()
+local sys = require 'luci.sys'
+local docker = require "luci.model.docker"
+
+m = SimpleForm("ubuntu", translate("ubuntu"), translate("Linkease-PC是为EasePi定制的一套Ubuntu系统。纯英文系统，欢迎各位极客玩家享用。")
+.. translatef(" "
+.. "<a href=\"%s\" target=\"_blank\">"
+.. "访问官网</a>", "https://easepi.linkease.com/"))
+m.submit=false
+m.reset=false
+
+s = m:section(SimpleSection)
+s.template = "dockerman/apply_widget"
+s.err = docker:read_status()
+s.err = s.err and s.err:gsub("\n","<br>"):gsub(" ","&nbsp;")
+if s.err then
+	docker:clear_status()
+end
+
+
+s=m:section(SimpleSection)
+s.template  = "ubuntu/ubuntu"
+
+
+return m
---
+++ applications/luci-app-ubuntu/root/usr/share/ubuntu/install.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+image_name=`uci get ubuntu.@ubuntu[0].image 2>/dev/null`
+
+[ -z "$image_name" ] && image_name="linkease/desktop-ubuntu-arm64:develop"
+
+install(){
+    local password=`uci get ubuntu.@ubuntu[0].password 2>/dev/null`
+    local port=`uci get ubuntu.@ubuntu[0].port 2>/dev/null`
+    [ -z "$password" ] && password="password"
+    [ -z "$port" ] && port=6901
+
+    docker run -d --name ubuntu \
+    --dns=223.5.5.5 -u=0:0 \
+    -v=/mnt:/mnt:rslave \
+    --net="docker-pcnet" \
+    --ip=10.10.100.9 \
+    --shm-size=512m \
+    -p $port:6901 \
+    -e VNC_PW=$password \
+    -e VNC_USE_HTTP=0 \
+    --restart unless-stopped \
+    $image_name
+
+}
+
+
+while getopts ":il" optname
+do
+    case "$optname" in
+        "l")
+        echo -n $image_name
+        ;;
+        "i")
+        install
+        ;;
+        ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+        "?")
+        echo "未知选项 $OPTARG"
+        ;;
+        *)
+       echo "Unknown error while processing options"
+        ;;
+    esac
+done
