b3d03cff713e5b484f59a048ea5014f40cef1c68
YGAS <ycl310397039@hotmail.com>
2021-12-24 16:13:10 +0800

    update ubuntu

--- applications/luci-app-ubuntu/root/usr/share/ubuntu/install.sh
+++ applications/luci-app-ubuntu/root/usr/share/ubuntu/install.sh
@@ -11,9 +11,11 @@ install(){
     [ -z "$port" ] && port=6901
 
     DOCKERPATH=`uci get dockerman.local.daemon_data_root`
-    if [[ findmnt -T ${DOCKERPATH} | grep -q /dev/sd ]] ; 
+    if findmnt -T ${DOCKERPATH} | grep -q /dev/sd ; 
+    then
         docker network ls -f "name=docker-pcnet" | grep -q docker-pcnet || \
         docker network create -d bridge --subnet=10.10.100.0/24 --ip-range=10.10.100.0/24 --gateway=10.10.100.1 docker-pcnet
+        
         docker run -d --name ubuntu \
         --dns=223.5.5.5 -u=0:0 \
         -v=/mnt:/mnt:rslave \
@@ -25,9 +27,9 @@ install(){
         -e VNC_USE_HTTP=0 \
         --restart unless-stopped \
         $image_name
-    then
+    else
         echo "docker not in disk"
-    fi                          
+    fi                         
 }
 
 
