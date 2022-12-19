2516fafc7442c396aecc3ef90420580c67baa815
jjm2473 <1129525450@qq.com>
2022-12-19 19:24:14 +0800

    fix depends

--- applications/luci-app-xunyou/luasrc/model/cbi/xunyou/main.lua
+++ applications/luci-app-xunyou/luasrc/model/cbi/xunyou/main.lua
@@ -3,7 +3,7 @@ local uci = luci.model.uci.cursor()
 local sys = require 'luci.sys'
 
 
-m = Map("xunyou", translate("xunyou"), translate("迅游是一款小巧且功能强大的网游加速器，迅游所采用的第五代网游加速技术能更有效地为您解决网游卡机、掉线、延时高、登录难等问题")
+m = Map("xunyou", translate("xunyou"), translate("迅游是一款小巧且功能强大的网游加速器，迅游所采用的第五代网游加速技术能更有效地为您解决网游卡机、掉线、延时高、登录难等问题。")
 .. translatef("更多信息请"
 .. "<a href=\"%s\" target=\"_blank\">"
 .. "访问官网</a>", "https://www.xunyou.com/"))
@@ -20,6 +20,5 @@ o.template = "cbi/network_netlist"
 o.nocreate = true
 o.default = "lan"
 o.datatype = "string"
-o:depends("enabled", 1)
 
 return m
