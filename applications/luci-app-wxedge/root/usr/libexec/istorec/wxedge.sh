74a95ec064a079789ede9fac6246f00b2ffe4f8e
YGAS <ycl310397039@hotmail.com>
2025-03-19 18:13:01 +0800

    Update wxedge.sh

--- applications/luci-app-wxedge/root/usr/libexec/istorec/wxedge.sh
+++ applications/luci-app-wxedge/root/usr/libexec/istorec/wxedge.sh
@@ -4,22 +4,24 @@ ACTION=${1}
 shift 1
 
 
-# 1. 判断是否安装 iStoreEnhance
-# 2. docker info获取Registry Mirrors:下的第一个服务器地址，并且地址包含registry.linkease.net
+# 1. 判断iStoreEnhance是否运行
+# 2. 使用 docker info 获取包含 registry.linkease.net 的镜像服务器地址
 # 3. 如果1和2都满足，则直接 docker pull registry.linkease.net:5443/onething1/wxedge
 # 4. 反之运行docker pull
 istoreenhance_pull() {
   local image_name="$1"
 
-  # 判断是否安装 iStoreEnhance
-  if command -v istoreenhance &> /dev/null; then
-    # 使用 docker info 获取镜像服务器列表
-    local registry_mirror=$(docker info 2>/dev/null | awk -F': ' '/Registry Mirrors:/ {getline; print $1}' | tr -d ' ' | awk -F',' '{print $1}')
-    if [[ -n "$registry_mirror" && "$registry_mirror" == *"registry.linkease.net"* ]]; then
+  # 判断iStoreEnhance是否运行
+  if pgrep "iStoreEnhance" >/dev/null; then
+    # 使用 docker info 获取包含 registry.linkease.net 的镜像服务器地址
+    local registry_mirror=$(docker info 2>/dev/null | awk -F': ' '/Registry Mirrors:/ {found=1; next} found && NF {if ($0 ~ /registry.linkease.net/) {print; exit}}')
+
+    if [[ -n "$registry_mirror" ]]; then
       # 提取主机和端口部分
       local registry_host=$(echo "$registry_mirror" | sed -E 's|^https?://([^/]+).*|\1|')
       # 拼接完整的镜像地址
       local full_image_name="$registry_host/$image_name"
+      echo "istoreenhance_pull ${full_image_name}"
       # 直接拉取镜像
       docker pull "$full_image_name"
       if [ $? -eq 0 ]; then
@@ -29,6 +31,7 @@ istoreenhance_pull() {
   fi
 
   # 否则运行 docker pull
+  echo "docker pull ${image_name}"
   docker pull "$image_name"
   if [ $? -ne 0 ]; then
     return 1
@@ -46,7 +49,6 @@ do_install() {
   fi
 
   [ -z "$image_name" ] && image_name="onething1/wxedge"
-  echo "docker pull ${image_name}"
   istoreenhance_pull "$image_name"
   if [ $? -ne 0 ]; then
     echo "download failed, install istoreenhance to speedup, \"https://doc.linkease.com/zh/guide/istore/software/istoreenhance.html\""
