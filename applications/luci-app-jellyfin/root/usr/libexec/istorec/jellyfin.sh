867f6c7386131d6e11caee3625bc6a06836ab816
YGAS <ycl310397039@hotmail.com>
2025-03-21 15:54:05 +0800

    Update jellyfin.sh

--- applications/luci-app-jellyfin/root/usr/libexec/istorec/jellyfin.sh
+++ applications/luci-app-jellyfin/root/usr/libexec/istorec/jellyfin.sh
@@ -17,27 +17,17 @@ istoreenhance_pull() {
   if [ -n "$isRun" ]; then
     # 使用 docker info 获取包含 registry.linkease.net 的镜像服务器地址
     local registry_mirror=$(docker info 2>/dev/null | awk -F': ' '/Registry Mirrors:/ {found=1; next} found && NF {if ($0 ~ /registry.linkease.net/) {print; exit}}')
-
-    if [[ -n "$registry_mirror" ]]; then
-      # 提取主机和端口部分
-      local registry_host=$(echo ${registry_mirror} | sed -E 's|^https?://([^/]+).*|\1|')
-      # 拼接完整的镜像地址
-      local full_image_name="$registry_host/$image_name"
-      echo "istoreenhance_pull ${full_image_name}"
-      # 直接拉取镜像
-      docker pull "$full_image_name"
-      if [ $? -ne 0 ]; then
+    echo "istoreenhance_pull ${image_name}"
+    docker pull "$image_name"
+    if [ $? -ne 0 ]; then
+      if [[ -n "$registry_mirror" ]]; then
         echo "istoreenhance_pull failed"
-      exit 1
+      else
+          echo "download failed, not found registry.linkease.net"
       fi
-    else
-      echo "istoreenhance_pull ${image_name}"
-      docker pull "$image_name"
-      if [ $? -ne 0 ]; then
-        echo "download failed, not found registry.linkease.net"
       exit 1
-      fi
     fi
+    
   else
     # 否则运行 docker pull
     echo "docker pull ${image_name}"
